84cef3df4a1f317a518c3f9dec17e57a9efdeef1
Oriol Martinez <2023_oriol.martinez@iticbcn.cat>
2023-12-03 12:09:04 +0100

    .

--- TableMeets/app/src/main/java/com/example/tablemeets/Home.kt
+++ TableMeets/app/src/main/java/com/example/tablemeets/Home.kt
@@ -1,12 +1,26 @@
 package com.example.tablemeets
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
-        setContentView(R.layout.home)
+
+        // Asigna el botón desde tu diseño XML a una variable en Kotlin
+        val botonIrATypeOfEvent = findViewById<Button>(R.id.button3)
+
+        // Asigna un OnClickListener al botón para manejar su clic
+        botonIrATypeOfEvent.setOnClickListener {
+            irATypeOfEvent()
+        }
+    }
+
+    private fun irATypeOfEvent() {
+        val intent = Intent(this, TypeOfEvent::class.java)
+        startActivity(intent)
     }
 }
