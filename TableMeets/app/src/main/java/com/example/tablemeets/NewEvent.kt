47f0dcea221a6972155729a1ab4d2a9ea6b0b796
oriol <2023_oriol.martinez@iticbcn.cat>
2023-12-09 21:22:52 +0100

    adding updates

--- TableMeets/app/src/main/java/com/example/tablemeets/NewEvent.kt
+++ TableMeets/app/src/main/java/com/example/tablemeets/NewEvent.kt
@@ -2,7 +2,10 @@ package com.example.tablemeets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import com.google.android.material.textfield.TextInputLayout
 
 class NewEvent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +17,13 @@ class NewEvent : AppCompatActivity() {
         val menuIcon = findViewById<ImageView>(R.id.menu_icon)
         val arrowBack = findViewById<ImageView>(R.id.left_arrow)
         val homeLogo = findViewById<ImageView>(R.id.home_logo)
+        val nameTextInput = findViewById<TextInputLayout>(R.id.NameTextInput)
+        val gameTextInput = findViewById<TextInputLayout>(R.id.GameTextInput)
+        val locationTextInput = findViewById<TextInputLayout>(R.id.LocationTextInput)
+        val timeTextInput = findViewById<TextInputLayout>(R.id.TimeTextInput)
+        val dateTextInput = findViewById<TextInputLayout>(R.id.DateTextInput)
+        val descTextInput = findViewById<TextInputLayout>(R.id.DescTextInput)
+        val removeData = findViewById<Button>(R.id.button_remove_all_inputs)
         //val createEvent = findViewById<ImageView>(R.id.home_logo)
 
         menuIcon.setOnClickListener {
@@ -32,12 +42,14 @@ class NewEvent : AppCompatActivity() {
             navigationHelper.goToHome()
         }
 
-
-
+        removeData.setOnClickListener {
+            nameTextInput.editText?.text?.clear()
+            gameTextInput.editText?.text?.clear()
+            locationTextInput.editText?.text?.clear()
+            timeTextInput.editText?.text?.clear()
+            dateTextInput.editText?.text?.clear()
+            descTextInput.editText?.text?.clear()
+        }
 
     }
-
-
-
-
 }
