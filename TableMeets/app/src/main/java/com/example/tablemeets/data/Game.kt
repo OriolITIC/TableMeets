6d0a3d0aa3b5a9ffcbddfae394f315190cabb279
Aqeedat Ghulam Nabi <145792532+Aqeedat-Nabi@users.noreply.github.com>
2024-03-20 18:48:05 +0100

    Delete TableMeets directory

--- TableMeets/app/src/main/java/com/example/tablemeets/data/Game.kt
+++
@@ -1,4 +0,0 @@
-package com.example.tablemeets.data
-
-class Game {
-}
--- TableMeets/app/src/main/java/com/example/tablemeets/data/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tablemeets.data
-
-data class User(
-    val id: Long,
-    val username: String,
-    val email: String,
-    val password: String
-)
